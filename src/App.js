4d15ffffb4a7922f0f785ef25005b67d11c41569
DESKTOP-7LN1193\niraj <nirajdahal09@gmail.com>
2022-12-27 23:18:51 +1100

    added initial folders

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+function App() {
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <main>This app is using the dark mode</main>
+      </ThemeProvider>
+    </>
+  )
+}
+export default App;
